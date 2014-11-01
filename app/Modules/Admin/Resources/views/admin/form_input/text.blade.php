eb4b165b0f612ad2f2cfab1ff9aba164b2061d32
Alexander Mikhailov <chrono@smtp.ru>
2014-11-02 04:44:47 +0600

    Adds name for inputs

--- app/Modules/Admin/Resources/views/admin/form_input/text.blade.php
+++ app/Modules/Admin/Resources/views/admin/form_input/text.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
 	<label for="input-text" class="col-sm-2 control-label">{{ $input['label'] }}</label>
 	<div class="col-sm-10">
-		<input type="text" class="form-control" id="input-text" placeholder="Введите текст" value="{{ $item->$name }}">
+		<input type="text" name="{{ $name }}" class="form-control" id="input-text" placeholder="Введите текст" value="{{ $item->$name }}">
 	</div>
 </div>
--- app/Modules/Admin/Resources/views/admin/form_input/textarea.blade.php
+++ app/Modules/Admin/Resources/views/admin/form_input/textarea.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
 	<label for="input-text" class="col-sm-2 control-label">{{ $input['label'] }}</label>
 	<div class="col-sm-10">
-	<textarea class="form-control" rows="10" placeholder="Input text">{{ $item->$name }}</textarea>
+	<textarea class="form-control" name="{{ $name }}" rows="10" placeholder="Input text">{{ $item->$name }}</textarea>
 	</div>
 </div>
