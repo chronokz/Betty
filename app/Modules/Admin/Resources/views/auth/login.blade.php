b87479db4409d1e366019f0a9ed0a7a61356cd36
Alexander Mikhailov <chrono@smtp.ru>
2014-11-15 16:41:12 +0600

    Add design for login page

--- app/Modules/Admin/Resources/views/auth/login.blade.php
+++ app/Modules/Admin/Resources/views/auth/login.blade.php
@@ -2,33 +2,34 @@
 
 @section('content')
 
-	<div class="col-md-4 col-md-offset-4 col-login">
-        {{ Form::open() }}
-            <h1 class="text-center">Login</h1>
-            <hr/>
-            @if(Session::has('error'))
-               <div class="alert alert-danger">
-                    {{ Session::get('error') }}
-                    <button type="button" class="close" data-dismiss="alert">
-                        <span aria-hidden="true">&times;</span>
-                        <span class="sr-only">Close</span>
-                    </button>
-               </div>
-            @endif
-            <div class="form-group">
-                {{ Form::label('username', 'Username:') }}
-                {{ Form::text('username', null, ['class' => 'form-control']) }}
-            </div>
-            <div class="form-group">
-                {{ Form::label('password', 'Password:') }}
-                {{ Form::password('password', ['class' => 'form-control']) }}
-            </div>
-            <div class="form-group">
-                {{ Form::submit('Login', ['class' => 'btn btn-block btn-primary']) }}
-            </div>
-            <hr/>
-            <a class="btn btn-block btn-default" href="{{ url('admin/register') }}">Register here</a>
-        {{ Form::close() }}
-    </div>
-
+<div class="box">
+	<div class="header clearfix">
+		<div class="pull-left"><i class="fa fa-sign-in"></i> Вход</div>
+	</div>
+	{{ Form::open() }}
+		@if(Session::has('error'))
+		   <div class="alert alert-danger no-radius no-margin padding-sm">
+				<i class="fa fa-info-circle"></i>
+				{{ Session::get('error') }}
+				<button type="button" class="close" data-dismiss="alert">
+					<span aria-hidden="true">&times;</span>
+					<span class="sr-only">Close</span>
+				</button>
+		   </div>
+		@endif
+		<div class="box-body padding-md">
+			<div class="form-group">
+				{{ Form::label('username', 'Логин:') }}
+				{{ Form::text('username', null, ['class' => 'form-control']) }}
+			</div>
+			<div class="form-group">
+				{{ Form::label('password', 'Пароль:') }}
+				{{ Form::password('password', ['class' => 'form-control']) }}
+			</div>
+			<div class="box-footer">                                                               
+				{{ Form::submit('Войти', ['class' => 'btn btn-block btn-primary']) }}
+			</div>
+		</div>
+	{{ Form::close() }}
+</div>
 @stop
--- app/Modules/Admin/Resources/views/layouts/auth.blade.php
+++ app/Modules/Admin/Resources/views/layouts/auth.blade.php
@@ -1,16 +1,81 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>@yield('title', 'Administrator')</title>
-	{{ admin_css('components/bootstrap/css/bootstrap.min.css') }}
-</head>
-<body>
+	<head>
+        <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+        <title>@yield('title', 'Administrator')</title>
+		
+		<!-- Stylesheets -->
+		{{ admin_css('css/bootstrap.min.css') }}
+		{{ admin_css('css/font-awesome.min.css') }}
+		{{ admin_css('css/animate/animate.min.css') }}
+		{{ admin_css('css/bootstrapValidator/bootstrapValidator.min.css') }}
+		{{ admin_css('css/iCheck/all.css') }}
+		{{ admin_css('css/style.css') }}
+		
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+			{{ admin_js('vendor/html5shiv/html5shiv.js') }}
+			{{ admin_js('vendor/respond/respond.min.js') }}
+        <![endif]-->
+	</head>
+	<body class="login fixed">
+		<div class="wrapper animated flipInY">
+			<div class="logo"><a href="#"><i class="fa fa-gears"></i> <span>Betty</span></a></div>
+			
+			@yield('content')
+			
+			<footer>
+				Developed by chrono &copy; {{ date('Y') }}.
+			</footer>
+		</div>
+		
+        <!-- Javascript -->
+        {{ admin_js('js/plugins/jquery/jquery-1.10.2.min.js') }}
 
-	<div class="container">
-		@yield('content')
-	</div>
-
-    {{ admin_js('js/jquery.min.js') }}
-    {{ admin_js('components/bootstrap/js/bootstrap.min.js') }}
-</body>
+        {{ admin_js('js/plugins/jquery/jquery-1.10.2.min.js') }}
+        {{ admin_js('js/plugins/jquery-ui/jquery-ui-1.10.4.min.js') }}
+		
+		<!-- Bootstrap -->
+        {{ admin_js('js/plugins/bootstrap/bootstrap.min.js') }}
+		
+		<!-- Interface -->
+        {{ admin_js('js/plugins/pace/pace.min.js') }}
+		
+		<!-- Forms -->
+        {{ admin_js('js/plugins/bootstrapValidator/bootstrapValidator.min.js') }}
+        {{ admin_js('js/plugins/iCheck/icheck.min.js') }}
+		
+		<script type="text/javascript">
+			//iCheck
+			$("input[type='checkbox'], input[type='radio']").iCheck({
+				checkboxClass: 'icheckbox_minimal',
+				radioClass: 'iradio_minimal'
+			});
+			
+			$(document).ready(function() {
+				$('#loginform').bootstrapValidator({
+					message: 'This value is not valid',
+					fields: {
+						username: {
+							message: 'The username is not valid',
+							validators: {
+								notEmpty: {
+									message: 'The username is required and can\'t be empty'
+								}
+							}
+						},
+						password: {
+							validators: {
+								notEmpty: {
+									message: 'The password is required and can\'t be empty'
+								}
+							}
+						}
+					}
+				});
+			});
+		</script>
+    </body>
 </html>
