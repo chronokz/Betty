6ddce62972499312bb9899186550d40371e74d40
Alexander Mikhailov <chrono@smtp.ru>
2016-01-02 22:46:21 +0600

    fix img in listing

--- app/Modules/Admin/Resources/views/admin/list_column/image.blade.php
+++ app/Modules/Admin/Resources/views/admin/list_column/image.blade.php
@@ -1,1 +1,5 @@
-{{ HTML::image($file_url.'/'.$name.'/admin_'.$item->$name) }}
+@if (file_exists($file_url.'/'.$name.'/admin_'.$item->$name))
+	{{ HTML::image($file_url.'/'.$name.'/admin_'.$item->$name) }}
+@else
+	{{ HTML::image(admin_asset('img/admin.jpg')) }}
+@endif
