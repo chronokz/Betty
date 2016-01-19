ec4ca3c6bb5dd6271735c78abe3e31ac6858cbd9
Alexander Mikhailov <chrono@smtp.ru>
2016-01-19 22:40:01 +0600

    Add more security

--- app/Modules/Admin/Http/filters.php
+++ app/Modules/Admin/Http/filters.php
@@ -2,7 +2,7 @@
 
 Route::filter('is_admin', function()
 {
-	if (! Auth::check())
+	if (!Auth::check() && (Auth::user()->group == 1 || Auth::user()->group == 2))
 	{
 		return Redirect::to('admin/login');
 	}
