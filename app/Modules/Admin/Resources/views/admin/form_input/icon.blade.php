a1950419ba955621ad570128ec1644eba8169b1d
Alexander Mikhailov <chrono@smtp.ru>
2016-04-25 22:19:39 +0600

    Fix saving icons input icon

--- app/Modules/Admin/Resources/views/admin/form_input/icon.blade.php
+++ app/Modules/Admin/Resources/views/admin/form_input/icon.blade.php
@@ -1,7 +1,7 @@
 <label class="col-sm-2 control-label">{{ $input['label'] }}</label>
 <div class="col-sm-10">
 	<div class="input-group">
-	  <span class="input-group-addon"><i id="icon_selector_view" style="font-size:18px" class="fa fa-{{ $input['options'][0] }}"></i></span>
+	  <span class="input-group-addon"><i id="icon_selector_view" style="font-size:18px" class="fa fa-{{ ($value)?$value:$input['options'][0] }}"></i></span>
 	  <select name="{{ $name }}" class="form-control" id="icon_selector">
 	  	@foreach ($input['options'] as $icon)
 	  		<option value="{{ $icon }}" @if($value == $icon) selected @endif>{{ $icon }}</option>
