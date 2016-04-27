27aa70c0259e657fffe97b0d2971a25f45beddbe
Alexander Mikhailov <chrono@smtp.ru>
2016-04-27 17:48:09 +0600

    Fix display image for listing

--- app/Modules/Admin/Resources/views/admin/list_column/image.blade.php
+++ app/Modules/Admin/Resources/views/admin/list_column/image.blade.php
@@ -1,4 +1,4 @@
-@if (@fopen($file_url.'/'.$name.'/admin_'.$item->$name, 'r'))
+@if ($item->$name && @fopen($file_url.'/'.$name.'/admin_'.$item->$name, 'r'))
 	{{ HTML::image($file_url.'/'.$name.'/admin_'.$item->$name) }}
 @else
 	{{ HTML::image(admin_asset('img/admin.jpg')) }}
