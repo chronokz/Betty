c853ed1fcedcf7b51fcb5a6d8fefeb2ec66466cc
Alexander Mikhailov <chrono@smtp.ru>
2016-05-14 19:10:27 +0600

    Add tooltips for buttons

--- app/Modules/Admin/Resources/views/admin/list_column/buttons.blade.php
+++ app/Modules/Admin/Resources/views/admin/list_column/buttons.blade.php
@@ -6,18 +6,6 @@
 		</a>
 	@endif
 
-	@if ($button == 'news')
-		<a class="btn btn-info btn-sm" href="">
-			<i class="fa fa-gavel"></i>  Ход строительства
-		</a>
-	@endif
-
-	@if ($button == 'rooms')
-		<a class="btn btn-info btn-sm" href="">
-			<i class="fa fa-cubes"></i>  Комнаты
-		</a>
-	@endif
-
 	@if ($button == 'features')
 		<a class="btn btn-info btn-sm" href="">
 			<i class="fa fa-modx"></i>  Особенности
@@ -38,20 +26,20 @@
 	
 
 	@if ($button == 'show')
-		<a class="btn btn-info btn-sm" href="{{ URL::route('admin.'.$module.'.show', $item->id) }}?{{ $_SERVER['QUERY_STRING'] }}" title="Посмотреть">
+		<a class="btn btn-info btn-sm" href="{{ URL::route('admin.'.$module.'.show', $item->id) }}?{{ $_SERVER['QUERY_STRING'] }}" data-toggle="tooltip" title="Посмотреть">
 			<i class="fa fa-eye "></i>  
 		</a>
 	@endif 
 
 	@if ($button == 'edit')
-		<a class="btn btn-warning btn-sm" href="{{ URL::route('admin.'.$module.'.edit', $item->id) }}?{{ $_SERVER['QUERY_STRING'] }}" title="Редактировать">
+		<a class="btn btn-warning btn-sm" href="{{ URL::route('admin.'.$module.'.edit', $item->id) }}?{{ $_SERVER['QUERY_STRING'] }}" data-toggle="tooltip" title="Редактировать">
 			<i class="fa fa-edit "></i>  
 		</a>
 	@endif 
 
 	@if ($button == 'delete')
 		{{ Form::open(array('url' => URL::route('admin.'.$module.'.destroy', $item->id), 'class' => 'delete-destroy', 'method' => 'DELETE')) }}
-			<a class="btn btn-danger btn-sm submit" title="Удалить">
+			<a class="btn btn-danger btn-sm submit" data-toggle="tooltip" title="Удалить">
 				<i class="fa fa-trash-o "></i> 
 			</a>
 		{{ Form::close() }}
