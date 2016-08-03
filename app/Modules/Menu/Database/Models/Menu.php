abc2f2fecb4d9fa7b2eba123b12daa8e7f26cbbb
Alexander Mikhailov <chrono@smtp.ru>
2016-08-03 23:50:33 +0600

    Add usefull methods for working with childrens of menu

--- app/Modules/Menu/Database/Models/Menu.php
+++ app/Modules/Menu/Database/Models/Menu.php
@@ -8,6 +8,23 @@ class Menu extends Model {
 
 	public $timestamps = false;
 
-    protected $fillable = ["text","link","position","parent_id","depth","left","right","public"];
+	protected $fillable = ["text","link","position","parent_id","depth","left","right","public"];
+    
+	public function children()
+	{
+		return $this->whereParentId($this->id)->orderBy('position')->get();
+	}
+
+	public static function hierarchy($parent_id = 0)
+	{
+		$items = self::whereParent_id($parent_id)->orderBy('position')->get();
+
+		foreach($items as $item)
+		{
+			$item->items = self::hierarchy($item->id);
+		}
+
+		return $items;
+	}
 
 }
