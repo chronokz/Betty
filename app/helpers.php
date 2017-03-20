e6880ca49d45eb363ed14647ce6a324c80decf14
Alexander Mikhailov <chrono@smtp.ru>
2017-03-20 13:16:02 +0600

    Add helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,61 @@
 <?php
+
+function lang_date($date)
+{
+	$lang = app()->getLocale();
+
+	$month['en'] = [
+		'january',
+		'february',
+		'march',
+		'april',
+		'may',
+		'june',
+		'july',
+		'august',
+		'september',
+		'october',
+		'november',
+		'december'
+	];
+
+	$month['ru'] = [
+		'января',
+		'февраля',
+		'марта',
+		'апреля',
+		'мая',
+		'июня',
+		'июля',
+		'августа',
+		'сентября',
+		'октября',
+		'ноября',
+		'декабря',
+	];
+
+	$month['kz'] = [
+		'қаңтар',
+		'ақпан',
+		'наурыз',
+		'сәуір',
+		'мамыр',
+		'маусым',
+		'шілде',
+		'тамыз',
+		'қыркүйек',
+		'қазан',
+		'қараша',
+		'желтоқсан',
+	];
+
+	return str_ireplace($month['en'], $month[$lang], $date);
+}
+
+function format_date($date, $format = 'j F Y')
+{
+	if ($date)
+		return lang_date(date($format, strtotime($date)));
+	else
+		return null;
+}
